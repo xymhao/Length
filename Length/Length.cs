e42fe4b36bbf5acc679f805fda83604ef9112eb5
xuyueming <xnysym@outlook.com>
2020-01-16 22:25:47 +0800

    添加项目文件。

---
+++ Length/Length.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Length
+{
+    public class Length
+    {
+        private readonly double value;
+        private readonly string unit;
+
+        public Length(double val, String uinnt)
+        {
+            value = val;
+            unit = uinnt;
+        }
+
+        public Length As(string u)
+        {
+            Length len = this;
+            if (unit.Equals("f"))
+            {
+                if (u.Equals("yard"))
+                {
+                    len = new Length(value / 3, u);
+                }
+                else if (u.Equals("inch"))
+                {
+                    len = new Length(value * 12, u);
+                }
+            }
+
+            if (unit.Equals("yard"))
+            {
+                if (u.Equals("inch"))
+                {
+                    len = new Length(value * 36, u);
+                }
+                else if (u.Equals("f"))
+                {
+                    len = new Length(value * 3, u);
+                }
+            }
+
+            if (unit.Equals("inch"))
+            {
+                if (u.Equals("f"))
+                {
+                    len = new Length(value / 12, u);
+                }
+                else if (u.Equals("yard"))
+                {
+                    len = new Length(value / 36, u);
+                }
+            }
+
+            return len;
+        }
+
+        public double getVal()
+        {
+            return value;
+        }
+
+        public String getUinnt()
+        {
+            return unit;
+        }
+    }
+}
---
+++ LengthTests/LengthTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LengthTests
+{
+    [TestClass]
+    public class LengthTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
