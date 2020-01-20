f6ea5639ce2fa84f439fac829bb39682e7c14b46
xuyueming <xnysym@outlook.com>
2020-01-20 22:17:54 +0800

    string to Enum

--- Length/Length.cs
+++ Length/Length.cs
@@ -16,46 +16,66 @@ namespace LengthDemo
             unit = uintValue;
         }
 
-        public Length As(string toUnit)
+
+        public Length As(string targetUnit)
         {
-            Length len = this;
+            if (targetUnit.Equals(Unit.Inch.ToString(), StringComparison.CurrentCultureIgnoreCase))
+            {
+                return As(targetUnit, Unit.Inch);
+            }
+
+            if (targetUnit.Equals(Unit.Feet.ToString(), StringComparison.CurrentCultureIgnoreCase))
+            {
+                return As(targetUnit, Unit.Feet);
+            }
+
+            if (targetUnit.Equals(Unit.Yard.ToString(), StringComparison.CurrentCultureIgnoreCase))
+            {
+                return As(targetUnit, Unit.Yard);
+            }
+            return As(targetUnit, null);
+        }
+
+        public Length As(string toUnit, Unit? toUnitEnum)
+        {
+            Length length = this;
             if (unit.Equals(Feet))
             {
-                if (toUnit.Equals(Yard))
+                if (toUnitEnum.Equals(Unit.Yard))
                 {
-                    len = new Length(value / 3, toUnit);
+                    length = new Length(value / 3, toUnit);
                 }
-                else if (toUnit.Equals(Inch))
+                else if (toUnitEnum.Equals(Unit.Inch))
                 {
-                    len = new Length(value * 12, toUnit);
+                    length = new Length(value * 12, toUnit);
                 }
             }
 
             if (unit.Equals(Yard))
             {
-                if (toUnit.Equals(Inch))
+                if (toUnitEnum.Equals(Unit.Inch))
                 {
-                    len = new Length(value * 36, toUnit);
+                    length = new Length(value * 36, toUnit);
                 }
-                else if (toUnit.Equals(Feet))
+                else if (toUnitEnum.Equals(Unit.Feet))
                 {
-                    len = new Length(value * 3, toUnit);
+                    length = new Length(value * 3, toUnit);
                 }
             }
 
             if (unit.Equals(Inch))
             {
-                if (toUnit.Equals(Feet))
+                if (toUnitEnum.Equals(Unit.Feet))
                 {
-                    len = new Length(value / 12, toUnit);
+                    length = new Length(value / 12, toUnit);
                 }
-                else if (toUnit.Equals(Yard))
+                else if (toUnitEnum.Equals(Unit.Yard))
                 {
-                    len = new Length(value / 36, toUnit);
+                    length = new Length(value / 36, toUnit);
                 }
             }
 
-            return len;
+            return length;
         }
 
         public double GetVal()
@@ -67,5 +87,12 @@ namespace LengthDemo
         {
             return unit;
         }
+    }
+
+    public enum Unit
+    {
+        Inch = 1,
+        Feet =12,
+        Yard = 36
     }
 }
--- LengthTests/LengthTests.cs
+++ LengthTests/LengthTests.cs
@@ -13,7 +13,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_1_Inch_Equals_1_Inch()
         {
-            Length result = new Length(1, Inch).As(Inch);
+            Length result = new Length(1, Inch).As(Inch, Unit.Inch);
 
             Assert.AreEqual(result.GetVal(), (1.0));
             Assert.AreEqual(result.GetUint(), (Inch));
@@ -22,7 +22,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_2_Feet_Equals_2_Feet()
         {
-            Length result = new Length(2, Feet).As(Feet);
+            Length result = new Length(2, Feet).As(Feet, Unit.Feet);
 
             Assert.AreEqual(result.GetVal(), (2.0));
             Assert.AreEqual(result.GetUint(), (Feet));
@@ -31,7 +31,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_1_Yard_Equals_1_Yard()
         {
-            Length result = new Length(1, Yard).As(Yard);
+            Length result = new Length(1, Yard).As(Yard,  Unit.Yard);
 
             Assert.AreEqual(result.GetVal(), (1.0));
             Assert.AreEqual(result.GetUint(), (Yard));
@@ -40,7 +40,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_1_Foot_Equals_12_Inches()
         {
-            Length result = new Length(1, Feet).As(Inch);
+            Length result = new Length(1, Feet).As(Inch, Unit.Inch);
 
             Assert.AreEqual(result.GetVal(), (12.0));
             Assert.AreEqual(result.GetUint(), (Inch));
@@ -49,7 +49,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_3_Foot_Equals_1_Yard()
         {
-            Length result = new Length(3, Feet).As(Yard);
+            Length result = new Length(3, Feet).As(Yard, Unit.Yard);
 
             Assert.AreEqual(result.GetVal(), (1.0));
             Assert.AreEqual(result.GetUint(), (Yard));
@@ -58,7 +58,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_1_Yard_Equals_3_Feet()
         {
-            Length result = new Length(1, Yard).As(Feet);
+            Length result = new Length(1, Yard).As(Feet, Unit.Feet);
 
             Assert.AreEqual(result.GetVal(), (3.0));
             Assert.AreEqual(result.GetUint(), (Feet));
@@ -67,7 +67,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_1_Yard_Equals_36_Inches()
         {
-            Length result = new Length(1, Yard).As(Inch);
+            Length result = new Length(1, Yard).As(Inch,Unit.Inch);
 
             Assert.AreEqual(result.GetVal(), (36.0));
             Assert.AreEqual(result.GetUint(), (Inch));
@@ -76,7 +76,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_2_yards_Equals_72_Inches()
         {
-            Length result = new Length(2, Yard).As(Inch);
+            Length result = new Length(2, Yard).As(Inch, Unit.Inch);
 
             Assert.AreEqual(result.GetVal(), (72.0));
             Assert.AreEqual(result.GetUint(), (Inch));
@@ -85,7 +85,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_12_Inches_Equals_1_Foot()
         {
-            Length result = new Length(12, Inch).As(Feet);
+            Length result = new Length(12, Inch).As(Feet, Unit.Feet);
 
             Assert.AreEqual(result.GetVal(), (1.0));
             Assert.AreEqual(result.GetUint(), (Feet));
@@ -94,7 +94,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_36_Inches_Equals_1_Yard()
         {
-            Length result = new Length(36, Inch).As(Yard);
+            Length result = new Length(36, Inch).As(Yard, Unit.Yard);
 
             Assert.AreEqual(result.GetVal(), (1.0));
             Assert.AreEqual(result.GetUint(), (Yard));
@@ -103,7 +103,7 @@ namespace LengthTests
         [TestMethod]
         public void Should_18_Inches_Equals_Half_Yard()
         {
-            Length result = new Length(18, Inch).As(Yard);
+            Length result = new Length(18, Inch).As(Yard, Unit.Yard);
 
             Assert.AreEqual(result.GetVal(), (0.5));
             Assert.AreEqual(result.GetUint(), (Yard));
